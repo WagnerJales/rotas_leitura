b478d8643fdde7d3b70daf8c4492166a7f77f5f7
WagnerJales <100356581+WagnerJales@users.noreply.github.com>
2025-04-03 10:39:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from folium import Choropleth, Circle, Marker, GeoJson
 from folium.plugins import MarkerCluster
 
 st.set_page_config(page_title="Gerador de Rotas por Cluster", layout="centered")
-st.title("Gerador de Rotas de Visita por Agrupamento")
+st.title("Gerador de Rotas Comerciais")
 
 # Entrada do usuário
 max_pontos_por_rota = st.number_input(
